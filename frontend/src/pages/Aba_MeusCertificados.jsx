68a571e15174f8a676abc575e8f7ac3a44bc672b
ArthurFernandes06 <artrdgs@gmail.com>
2026-06-07 16:15:38 -0300

    Aba Meus certificados consome a api agora

--- frontend/src/pages/Aba_MeusCertificados.jsx
+++ frontend/src/pages/Aba_MeusCertificados.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import lupa from '../assets/imagens/lupa.png'
 import add from '../assets/imagens/adicionar_certificados.png'
 import editar from '../assets/imagens/download.png'
 import excluir from '../assets/imagens/excluirparticipante.png'
 import sair from '../assets/imagens/sair.png'
+import { listarCertificadosAPI } from '../services/certificados'
 
 export default function Aba_Participantes({voltarAbaLobby})
 {
@@ -16,22 +17,18 @@ const [buscanalista, setBuscaNaLista] = useState(null);
 
 // A LISTA É RESULTADO DE UMA BUSCA DE TODOS OS CERTIFICADOS QUE CONTÉM
 // COMO ID DE USUÁRIO O MESMO QUE O ID DO USUÁRIO LOGADO
-const listaDeTeste = [
-  { id: 1, id_usuario: 12, id_evento: 155, evento: "Evento 1", horas: 20 },
-  { id: 2, id_usuario: 13, id_evento: 157, evento: "GPEC", horas: 30 },
-  { id: 3, id_usuario: 14, id_evento: 156, evento: "Coding", horas: 40 },
-  { id: 4, id_usuario: 15, id_evento: 113, evento: "Evento 2", horas: 50 },
-  { id: 5, id_usuario: 16, id_evento: 191, evento: "Evento 3", horas: 60 },
-  { id: 6, id_usuario: 17, id_evento: 187, evento: "Evento 4", horas: 70 },
-  { id: 7, id_usuario: 18, id_evento: 104, evento: "Evento 5", horas: 80 },
-];
+const [listaCertificados, setListaCertificados] = useState([]);
+
+useEffect(() => {
+    listarCertificadosAPI(setListaCertificados);
+}, []);
 
 const procurarParticipantes = (e) => {
   e.preventDefault();
   if (nomeouevento !== '')
     {setProcurando(true);
-     const busca = listaComNomes.filter((user) => {
-        const achouEvento = user.evento.toLowerCase().includes(nomeouevento.toLowerCase());
+     const busca = listaCertificados.filter((user) => {
+        const achouEvento = user.titulo_evento.toLowerCase().includes(nomeouevento.toLowerCase());
         return achouEvento})
      setBuscaNaLista(busca);
      if (busca.length === 0)
@@ -115,6 +112,7 @@ return (
     </div>
     
     <div style={{maxWidth: '1000px',
+
                  margin: '0 auto',}}>
         <div style = {{userSelect: 'none',
                        marginTop:'50px',
@@ -132,13 +130,13 @@ return (
                    maxWidth: '1000px',
                    margin: '0 auto',}}>
 
-        {!procurando && listaDeTeste.map((user) => (
-          <div key={user} style={{display: 'flex', 
+        {!procurando && listaCertificados.map((user) => (
+          <div key={user.id} style={{display: 'flex', 
                                      alignItems: 'center',
                                      padding: '10px 0px',
                                      borderBottom: '3px solid #ccc'}}>
-            <div className = 'nomeFlex'>{user.evento}</div>
-            <div className = 'nomeFlex'>{user.horas}h</div>
+            <div className = 'nomeFlex'>{user.titulo_evento}</div>
+            <div className = 'nomeFlex'>{user.carga_horaria}h</div>
               <div style={{flex: 1}}>
                 <button onClick = {() => download(user)}
                         style={{userSelect: 'none',
@@ -156,8 +154,8 @@ return (
                                      alignItems: 'center',
                                      padding: '10px 0px',
                                      borderBottom: '3px solid #ccc'}}>
-            <div className = 'nomeFlex'>{user.evento}</div>
-            <div className = 'nomeFlex'>{user.horas}h</div>
+            <div className = 'nomeFlex'>{user.titulo_evento}</div>
+            <div className = 'nomeFlex'>{user.carga_horaria}h</div>
             <div style={{flex: 1}}>
               <button onClick = {() => deletartrue(user)}
                       style={{userSelect: 'none',
